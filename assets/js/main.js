e0a9399ccdd323e3a19ea6f02d4096479011c666
akintadetemi19@gmail.com <akintadetemi19@gmail.com>
2023-08-25 00:39:45 +0100

    game functionalities implemented, other features to be added

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,15 @@ const tiles = document.querySelectorAll('#container div')
 const modal = document.querySelector('#modal')
 let diff1 = 50
 let diff2 = 50
+let player1, player2
+
+let checkHeight = (ind, name) => {
+	if (tiles[ind].style.height === '100%'){
+		createModal(`
+			${name} wins!
+		`)
+	}
+}
 
 tiles.forEach((val, ind) => {
 	val.style.height = '50%'
@@ -12,6 +21,7 @@ tiles.forEach((val, ind) => {
 				diff2 -= 5;
 				tiles[0].style.height = `${diff1}%`
 				tiles[1].style.height = `${diff2}%`
+				checkHeight(0, player1)
 				break;
 				
 			case 1:
@@ -19,8 +29,8 @@ tiles.forEach((val, ind) => {
 				diff2 += 5;
 				tiles[0].style.height = `${diff1}%`
 				tiles[1].style.height = `${diff2}%`
+				checkHeight(1, player2)
 				break;
 		}
 	}
 })
-
--- assets/js/ui.js
+++ assets/js/ui.js
@@ -1,3 +1,10 @@
 const createModal = (content) => {
+	modal.animate({
+		transform: ['scale(0)', 'scale(1.05)', 'scale(1)']
+	},
+	{
+		duration: 500
+	})
+	modal.parentElement.style.display = 'block'
 	modal.innerHTML = content
 }
